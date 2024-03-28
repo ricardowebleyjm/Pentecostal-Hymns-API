9759bf971bbdcb20ab00cd624571e0523f544d07
Ricardo Webley <rwebley@duck.com>
2024-03-28 11:56:41 -0500

    implement search

--- hymns/api.py
+++ hymns/api.py
@@ -18,8 +18,17 @@ def hymns_list(request: HttpRequest):
     return Hymn.objects.all()
 
 
+@router.get("/search/", response=List[HymnSchema])
+def search_by_title(request: HttpRequest, hymn_title: str):
+    """Search Hymn by title"""
+    hymns = Hymn.objects.filter(title__icontains=hymn_title)
+    serialized_hymns = [HymnSchema.from_orm(hymn) for hymn in hymns]
+    return serialized_hymns
+
+
 @router.get("/{hymn_id}/", response=HymnSchema)
 def get_hymn_by_id(request: HttpRequest, hymn_id:int):
     """Returns a single by ID"""
     hymn = get_object_or_404(Hymn, pk=hymn_id)
     return hymn
+    
